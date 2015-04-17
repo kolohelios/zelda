783bd5b15b9f17a40a3c170c429661510a1bbfa7
Jon Edwards <jkedwards@me.com>
2015-04-17 13:21:43 -0700

    add paint avatar

--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 'use strict';
-/* globals: Firebase true */
+/* global Firebase */
 
-var root, characters;
+var root, characters, myKey, myCharacter;
 
 $(document).ready(init);
 
@@ -12,7 +12,10 @@ function init(){
   $('#login-user').click(loginUser);
   $('#logout-user').click(logoutUser);
   $('#create-character').click(createCharacter);
+  $('#start-user').click(startUser);
+  $(document).keydown(keyDown);
   characters.on('child_added', characterAdded);
+  characters.on('child_changed', characterChanged);
 }
 
 function createUser(){
@@ -62,18 +65,19 @@ function characterAdded(snapshot){
   var character = snapshot.val();
   var myUid = root.getAuth() ? root.getAuth().uid : '';
   var active = '';
-  console.log(myUid);
   if(myUid === character.uid){
+    myKey = snapshot.key();
+    myCharacter = snapshot.val();
     active = 'active';
   }
   var tr =  '<tr class="' + active + '"><td>' + character.handle + '</td><td><img src="' + character.avatar + '"></td></tr>';
   $('#characters > tbody').append(tr);
-  console.log(character);
 }
 
 function logoutUser(){
   console.log('running');
   root.unauth();
+  myKey = null;
   $('#characters > tbody > tr.active').removeClass('active');
 }
 
@@ -81,4 +85,38 @@ function redrawCharacters(){
   $('#characters > tbody').empty();
   characters.off('child_added', characterAdded);
   characters.on('child_added', characterAdded);
+}
+
+function startUser(){
+  var spaceHasNotBeenCleared = true, x, y;
+  while(spaceHasNotBeenCleared){
+    x = Math.floor(Math.random() * 10);
+    y = Math.floor(Math.random() * 10);
+    var $charLoc = $('#board td[data-x="' + x + '"][data-y="' + y + '"]');
+    if(!($charLoc.hasClass('avatar'))){
+      spaceHasNotBeenCleared = false;
+    }
+  }
+  characters.child(myKey).update({
+    x: x,
+    y: y
+  });
+}
+
+function characterChanged(snapshot){
+  var character = snapshot.val();
+  if($('.avatar-' + character.handle).length > 0){
+    $('.avatar-' + character.handle).css('background-image', '');
+    $('.avatar-' + character.handle).removeClass('avatar ' + 'avatar-' + character.handle);
+  }
+  var $charLoc = $('#board td[data-x="' + character.x + '"][data-y="' + character.y + '"]');
+
+  $charLoc.css('background-image', 'url("' + character.avatar + '")').addClass('avatar ' + 'avatar-' + character.handle);
+}
+
+function keyDown(event){
+  var keyCode = event.keyCode;
+
+  $('.avatar-zool').data('x');
+
 }
