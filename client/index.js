1b6d32f758310a43393cfe42c915b29e5f694c2d
Jon Edwards <jkedwards@me.com>
2015-04-16 18:41:13 -0700

    wire things up

--- client/index.js
+++ client/index.js
@@ -1,1 +1,80 @@
 'use strict';
+
+var root, characters;
+
+$(document).ready(init);
+
+function init(){
+  root = new Firebase('https://zelda-kolohelios.firebaseio.com/');
+
+  characters = root.child('characters');
+  $('#create-user').click(createUser);
+  $('#login-user').click(loginUser);
+  $('#create-character').click(createCharacter);
+  characters.on('child_added', characterAdded);
+}
+
+function createUser(){
+  var email = $('#email').val();
+  var password = $('#password').val();
+
+  root.createUser({
+    email    : email,
+    password : password
+  }, function(error, userData) {
+    if (error) {
+      console.log("Error creating user:", error);
+    } else {
+      console.log("Successfully created user account with uid:", userData.uid);
+    }
+  });
+}
+
+function loginUser(){
+  var email = $('#email').val();
+  var password = $('#password').val();
+
+  root.authWithPassword({
+    email    : email,
+    password : password
+  }, function(error, authData) {
+    if (error) {
+      console.log("Login Failed!", error);
+    } else {
+      console.log("Authenticated successfully with payload:", authData);
+    }
+  });
+}
+
+function createCharacter(){
+  var handle = $('#handle').val();
+  var avatar= $('#avatar').val();
+  var uid = root.getAuth().uid;
+
+  characters.push({
+    handle: handle,
+    avatar: avatar,
+    uid: uid
+  });
+
+}
+
+function characterAdded(snapshot){
+  var character = snapshot.val();
+  var myUid = root.getAuth().uid;
+  var active = '';
+
+  console.log(myUid);
+  if(myUid === character.uid){
+    active = 'active';
+  }
+
+
+
+  var tr =  '<tr class="' + active + '"><td>' + character.handle + '</td><td><img src="' + character.avatar + '"></td></tr>';
+  $('#characters > tbody').append(tr);
+
+
+
+  console.log(character);
+}
