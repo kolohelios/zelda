c933d40da782fad02bf844823b927ca4f2f08897
Jon Edwards <jkedwards@me.com>
2015-04-17 15:29:17 -0700

    add primitive item drops and handling

--- client/index.js
+++ client/index.js
@@ -1,13 +1,20 @@
 'use strict';
 /* global Firebase */
 
-var root, characters, myKey, myCharacter;
+var root, characters, items, myKey, myCharacter;
+
+var moveSnd = '/assets/blip.wav';
+var blackholeSnd = '/assets/blackhole.wav';
+var knifeSnd = '/assets/knife.wav';
+var healthSnd = '/assets/health.wav';
+var $sound;
 
 $(document).ready(init);
 
 function init(){
   root = new Firebase('https://zelda-kolohelios.firebaseio.com/');
   characters = root.child('characters');
+  items = root.child('items');
   $('#create-user').click(createUser);
   $('#login-user').click(loginUser);
   $('#logout-user').click(logoutUser);
@@ -15,7 +22,11 @@ function init(){
   $('#start-user').click(startUser);
   $(document).keydown(keyDown);
   characters.on('child_added', characterAdded);
+  items.on('child_added', itemAdded);
   characters.on('child_changed', characterChanged);
+  items.on('child_removed', itemRemoved);
+  $sound = $('#sound');
+  startTimer();
 }
 
 function createUser(){
@@ -67,7 +78,7 @@ function characterAdded(snapshot){
   var active = '';
   if(myUid === character.uid){
     myKey = snapshot.key();
-    myCharacter = snapshot.val();
+    myCharacter = character;
     active = 'active';
   }
   var tr =  '<tr class="' + active + '"><td>' + character.handle + '</td><td><img src="' + character.avatar + '"></td></tr>';
@@ -115,8 +126,85 @@ function characterChanged(snapshot){
 }
 
 function keyDown(event){
-  var keyCode = event.keyCode;
+  event.preventDefault();
+  var x = $('.avatar-' + myCharacter.handle).data('x');
+  var y = $('.avatar-' + myCharacter.handle).data('y');
 
-  $('.avatar-zool').data('x');
+  switch (event.keyCode){
+    case 37:
+      x -= 1;
+      break;
+    case 38:
+      y -= 1;
+      break;
+    case 39:
+      x += 1;
+      break;
+    case 40:
+      y += 1;
+  }
+  x = Math.min(x, 9);
+  x = Math.max(x, 0);
+  y = Math.min(y, 9);
+  y = Math.max(y, 0);
+
+  characters.child(myKey).update({x: x, y: y});
+
+  var $loc = $('#board td[data-x="' + x + '"][data-y="' + y + '"]');
+  var key = $loc.data('key');
+
+  if($loc.hasClass('health')){
+    playSound(healthSnd);
+    items.child(key).remove();
+  } else if($loc.hasClass('blackhole')){
+    playSound(blackholeSnd);
+    items.child(key).remove();
+  } else if($loc.hasClass('weapon')){
+    playSound(knifeSnd);
+    items.child(key).remove();
+  } else{
+    playSound(moveSnd);
+  }
+}
+
+function startTimer(){
+  setInterval(dropItems, 10000);
+}
+
+function dropItems(){
+  var names = ['health', 'weapon', 'blackhole'];
+  var rnd = Math.floor(Math.random() * names.length);
+  var name = names[rnd];
+
+  var x = Math.floor(Math.random() * 10);
+  var y = Math.floor(Math.random() * 10);
+
+  items.push({
+    name: name,
+    x: x,
+    y: y
+  });
+}
+
+function itemAdded(snapshot){
+  var drop = snapshot.val();
+  var key = snapshot.key();
+  var item = drop.name;
+  var x = drop.x;
+  var y = drop.y;
+  console.log('item', item, 'x', x, 'y', y);
+  var $itemLoc = $('#board td[data-x="' + x + '"][data-y="' + y + '"]');
+  $itemLoc.addClass(item);
+  $itemLoc.attr('data-key', key);
+}
+
+function playSound(sound){
+  $sound.attr('src', sound);
+  $sound[0].play();
+}
+
+function itemRemoved(snapshot){
+  var key = snapshot.key();
+  $('#board td[data-key="' + key + '"]').removeClass('health weapon blackhole').attr('data-key', '');
 
 }
